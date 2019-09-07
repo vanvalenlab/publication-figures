2cfd18709b5f9c0800f0ff45b549fc940241c887
dylanbannon <dylanbannon5@gmail.com>
2019-09-07 12:58:20 -0800

    linting

--- graph_creation.py
+++ graph_creation.py
@@ -1,4 +1,10 @@
 # graph_creation.py
+"""
+graph_creation.py
+
+The purpose of this script is to coordinate the production of the publication figures.
+"""
+
 
 import logging
 from os import path, getcwd
@@ -7,84 +13,96 @@ from graph_scripts import utils
 from graph_scripts.utils import MissingDataError
 from graph_scripts.figures import ImageTimeVsGpuFigure, BulkTimeVsGpuFigure, CostVsGpuFigure
 
-def main():
-    raw_data = utils.extract_data(input_folder)
+def create_figures():
+    """
+    This function takes in the user-specified parameters and coordinates
+    the creation of publicatoin figures. Please view the `if __name__ == __main__:`
+    block in the source code to view or set parameters.
+    """
 
-    for chosen_delay in delays:
+    raw_data = utils.extract_data(INPUT_FOLDER)
+
+    for chosen_delay in DELAYS:
         # create multiple-image-number graph
-        if create_bulk_time_vs_gpu_figure:
+        if CREATE_BULK_TIME_VS_GPU_FIGURE:
             # num_gpu vs. time plot
-            time_vs_gpu_figure = BulkTimeVsGpuFigure(raw_data, chosen_delay, img_nums, output_folder)
+            time_vs_gpu_figure = BulkTimeVsGpuFigure(
+                raw_data, chosen_delay, IMG_NUMS, OUTPUT_FOLDER)
             try:
                 time_vs_gpu_figure.plot()
                 logging.debug(f"Saved {time_vs_gpu_figure.plot_pdf_name}")
             except MissingDataError:
-                logging.error(f"No data for gpu vs time for delay {chosen_delay} and multiple image conditions.")
-        
+                logging.error(
+                    f"No data for gpu vs time for "
+                    f"delay {chosen_delay} and multiple image conditions.")
+
         # create single-image-number graphs
-        for chosen_img_num in img_nums:
+        for chosen_img_num in IMG_NUMS:
             logging.info(f"Delay: {chosen_delay}, number of images: {chosen_img_num}")
             chosen_img_str = str(chosen_img_num)
-            if len(chosen_img_str)>=7:
+            if len(chosen_img_str) >= 7:
                 pdf_label = chosen_img_str[:-6] + "m"
-            elif len(chosen_img_str)>=4:
+            elif len(chosen_img_str) >= 4:
                 pdf_label = chosen_img_str[:-3] + "k"
             else:
                 raise TypeError("Is this a number of the right length?")
 
-            if create_image_time_vs_gpu_figure:
+            if CREATE_IMAGE_TIME_VS_GPU_FIGURE:
                 # num_gpu vs. image time plot
-                image_time_vs_gpu_figure = ImageTimeVsGpuFigure(raw_data, chosen_delay, chosen_img_num, pdf_label, output_folder)
+                image_time_vs_gpu_figure = ImageTimeVsGpuFigure(
+                    raw_data, chosen_delay, chosen_img_num, pdf_label, OUTPUT_FOLDER)
                 try:
                     image_time_vs_gpu_figure.plot()
                     logging.debug(f"Saved {image_time_vs_gpu_figure.plot_pdf_name}")
                 except MissingDataError:
-                    logging.error(f"No data for gpu vs image time for delay {chosen_delay} and img_num {chosen_img_num}.")
-            if create_cost_vs_gpu_figure:
+                    logging.error(
+                        f"No data for gpu vs image time for "
+                        f"delay {chosen_delay} and img_num {chosen_img_num}.")
+            if CREATE_COST_VS_GPU_FIGURE:
                 # num_gpu vs. cost plot
-                cost_vs_gpu_figure = CostVsGpuFigure(raw_data, chosen_delay, chosen_img_num, pdf_label, output_folder)
+                cost_vs_gpu_figure = CostVsGpuFigure(
+                    raw_data, chosen_delay, chosen_img_num, pdf_label, OUTPUT_FOLDER)
                 try:
                     cost_vs_gpu_figure.plot()
                     logging.debug(f"Saved {cost_vs_gpu_figure.plot_pdf_name}")
                 except MissingDataError:
-                    logging.error(f"No data for gpu vs cost for delay {chosen_delay} and img_num {chosen_img_num}.")
+                    logging.error(
+                        f"No data for gpu vs cost for "
+                        f"delay {chosen_delay} and img_num {chosen_img_num}.")
 
-if __name__=='__main__':
+def configure_logging():
+    """This function configures logging for the whole module."""
 
-    # configuration parameters
-    input_folder = "new_data"
-    output_folder = "outputs"
-    delays = [5.0]
-    #delays = [0.5, 5.0]
-    img_nums = [10000, 100000, 1000000]
-    create_image_time_vs_gpu_figure = True
-    create_bulk_time_vs_gpu_figure = True
-    create_cost_vs_gpu_figure = True
-
-    # configure logging
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+    LOGGER.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    
-    # validate configuration parameters
-    if input_folder[0]=="/":
-        input_folder = input_folder
-    else:
-        input_folder = path.join(getcwd(),input_folder)
-    if output_folder[0]=="/":
-        output_folder = output_folder
-    else:
-        output_folder = path.join(getcwd(),output_folder)
-    logging.debug(f"Input directory: {input_folder}, Output directory: {output_folder}")
-    assert path.isdir(input_folder)
-    assert path.isdir(output_folder)
+    LOGGER.addHandler(ch)
 
-    # create plots
-    main()
-    
-    if False:
-        import pdb; pdb.set_trace()
+if __name__ == '__main__':
+
+    # configuration parameters
+    INPUT_FOLDER = "new_data"
+    INPUT_FOLDER = "outputs"
+    DELAYS = [5.0]
+    #DELAYS = [0.5, 5.0]
+    IMG_NUMS = [10000, 100000, 1000000]
+    CREATE_IMAGE_TIME_VS_GPU_FIGURE = True
+    CREATE_BULK_TIME_VS_GPU_FIGURE = True
+    CREATE_COST_VS_GPU_FIGURE = True
+
+    # configure logging
+    LOGGER = logging.getLogger(__name__)
+
+    # validate configuration parameters
+    if INPUT_FOLDER[0] != "/":
+        INPUT_FOLDER = path.join(getcwd(), INPUT_FOLDER)
+    if OUTPUT_FOLDER[0] != "/":
+        OUTPUT_FOLDER = path.join(getcwd(), OUTPUT_FOLDER)
+    logging.debug(f"Input directory: {INPUT_FOLDER}, Output directory: {OUTPUT_FOLDER}")
+    assert path.isdir(INPUT_FOLDER)
+    assert path.isdir(OUTPUT_FOLDER)
+
+    # create figures
+    create_figures()
