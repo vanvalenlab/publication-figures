64d83ee798d4c60cee1960a35adcea1b21bb9509
dylanbannon <dylanbannon5@gmail.com>
2019-09-06 18:42:51 -0800

    cleaning up graph_creation.py

--- graph_creation.py
+++ graph_creation.py
@@ -9,188 +9,14 @@
 # 1) pass data structures off to each image creation function, as appropriate
 
 
-import json
-from os import listdir
-from os.path import isfile, join
-import re
 import numpy as np
 import pandas as pd
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
 import pdb
 
-from graph_scripts.utils import MissingDataError
+from graph_scripts.utils import NoGpuError, MissingDataError, define_colorblind_color_map, format_data_for_error_plotting, extract_data
 from graph_scripts.figures import ImageTimeVsGpu
-
-from numpy.core._exceptions import UFuncTypeError
-
-# custom execption for a failing regex
-class NoGpuError(Exception):
-    pass
-
-# custom execption for missing data
-#class MissingDataError(Exception):
-#    pass
-
-# utility function:
-# https://stackoverflow.com/questions/3229419/how-to-pretty-print-nested-dictionaries
-def pretty(d, indent=0):
-   for key, value in d.items():
-      print('\t' * indent + str(key))
-      if isinstance(value, dict):
-         pretty(value, indent+1)
-      else:
-         print('\t' * (indent+1) + str(value))
-
-def extract_data():
-    data_dir = "/home/linus/projects/vanvalen_kiosk/figure-generation/new_data"
-    data_files = [f for f in listdir(data_dir) if isfile(join(data_dir, f)) and f.endswith(".json")]
-
-    # extract data from json files
-    data_keys = ['cpu_node_cost', 'gpu_node_cost', 'total_node_and_networking_costs', 'start_delay', 'num_jobs', 'time_elapsed']
-    aggregated_data = []
-    total_successes = 0 # these are zip files where all images succeeded
-    total_failures = 0 # these are zip files which had at least one failing image and, thus, did not complete
-    for data_file in data_files:
-        file_path = join(data_dir, data_file)
-        with open(file_path, "r") as open_file:
-            json_data = json.load(open_file)
-            data_to_keep = {}
-            for data_key in data_keys:
-                if "cost" in data_key:
-                    # removing string datatypes
-                    data_to_keep[data_key] = float(json_data[data_key])
-                elif "time_elapsed" in data_key:
-                    # converting from seconds to minutes
-                    data_to_keep[data_key] = json_data[data_key]/60
-                else:
-                    data_to_keep[data_key] = json_data[data_key]
-                # adding in extra data
-                num_images = len(json_data['job_data'])*100
-                data_to_keep['num_images'] = num_images
-                try:
-                    num_gpus = re.search('^([0-9]+)gpu', data_file).group(1)
-                except AttributeError:
-                    raise NoGpuError(f"Whoops, no gpus in filename {data_file}.")
-                data_to_keep['num_gpus'] = int(num_gpus)
-            # replacing total cost data
-            network_costs = extra_network_costs(data_to_keep['num_images'], data_to_keep['time_elapsed'])
-            data_to_keep['extra_network_costs'] = network_costs
-            data_to_keep['total_node_and_networking_costs'] = \
-                    data_to_keep['cpu_node_cost'] + \
-                    data_to_keep['gpu_node_cost'] + \
-                    data_to_keep['extra_network_costs']
-            # compile all file-level time components and compute some aggregate properties
-            total_time = 0
-            total_upload_time = 0
-            total_prediction_time = 0
-            total_postprocess_time = 0
-            total_download_time = 0
-            all_total_times = []
-            all_upload_times = []
-            all_prediction_times = []
-            all_postprocess_times = []
-            all_download_times = []
-            for i in range(json_data['num_jobs']):
-                try:
-                    # TODO: potentially pad all data?
-                    # gather raw times
-                    total_times = json_data['job_data'][i]['total_time']
-                    upload_times = json_data['job_data'][i]['upload_time']
-                    prediction_times = json_data['job_data'][i]['prediction_time']
-                    postprocess_times = json_data['job_data'][i]['postprocess_time']
-                    download_times = json_data['job_data'][i]['download_time']
-                    # append to compilation lists
-                    time_lists = [total_times, upload_times, prediction_times, postprocess_times, download_times]
-                    all_time_lists = [all_total_times, all_upload_times, all_prediction_times, all_postprocess_times, all_download_times]
-                    for time_list, all_time_list in zip(time_lists, all_time_lists):
-                        for entry in time_list:
-                            all_time_list.append(entry)
-                    # increment sums
-                    total_time = total_time + sum(total_times)
-                    total_upload_time = total_upload_time + sum(upload_times)
-                    total_prediction_time = total_prediction_time + sum(prediction_times)
-                    total_postprocess_time = total_postprocess_time + sum(postprocess_times)
-                    total_download_time = total_download_time + sum(download_times)
-                    # register a success
-                    total_successes = total_successes + 1
-                except TypeError:
-                    print("Error.")
-                    total_failures = total_failures + 1
-            data_to_keep['total_image_time'] = total_time/60
-            data_to_keep['total_image_upload_time'] = total_upload_time/60
-            data_to_keep['total_image_prediction_time'] = total_prediction_time/60
-            data_to_keep['total_image_postprocess_time'] = total_postprocess_time/60
-            data_to_keep['total_image_download_time'] = total_download_time/60
-            data_to_keep['average_image_time'] = data_to_keep['total_image_time']/total_successes
-            data_to_keep['average_image_upload_time'] = data_to_keep['total_image_upload_time']/total_successes
-            data_to_keep['average_image_prediction_time'] = data_to_keep['total_image_prediction_time']/total_successes
-            data_to_keep['average_image_postprocess_time'] = data_to_keep['total_image_postprocess_time']/total_successes
-            data_to_keep['average_image_download_time'] = data_to_keep['total_image_download_time']/total_successes
-            data_to_keep['all_total_times'] = all_total_times
-            upload_download_multiplier = 2
-            # We need to wrap all this in a while loop because we might hit multiple exceptions in series.
-            while True:
-                try:
-                    data_to_keep['all_network_times'] = np.add( np.dot(upload_download_multiplier,all_upload_times), np.dot(upload_download_multiplier,all_download_times))
-                    break
-                # Sometimes the upload and download series would have slightly different numbers of entries.
-                # This is likely due to Redis not yet having the data on rare occasions when we ask for it.
-                # We don't consider this to be a huge concern and note that it only marginally affects the
-                # completeness of our data. Further, it doesn't bias our results.
-                except ValueError:
-                    upload_length = len(all_upload_times)
-                    download_length = len(all_download_times)
-                    if upload_length < download_length:
-                        upload_average = np.average(all_upload_times)
-                        length_diff = download_length - upload_length
-                        for i in range(length_diff):
-                            all_upload_times = np.append(all_upload_times, upload_average)
-                    elif download_length < upload_length:
-                        download_average = np.average(all_download_times)
-                        length_diff = upload_length - download_length
-                        for i in range(length_diff):
-                            all_download_times = np.append(all_download_times, download_average)
-                    else:
-                        raise ValueError
-                # We had some entries that were text??? Very weird and I want to make sure this wasn't an issue.
-                except UFuncTypeError as e:
-                    for i, upload in enumerate(all_upload_times):
-                        if type(upload)==str:
-                            all_upload_times[i] = np.nan
-                    upload_mean = np.nanmean(all_upload_times)
-                    for i, upload in enumerate(all_upload_times):
-                        if np.isnan(upload):
-                            all_upload_times[i] = upload_mean
-                    for i, download in enumerate(all_download_times):
-                        if type(download)==str:
-                            all_download_times[i] = np.nan
-                    download_mean = np.nanmean(all_download_times)
-                    for i, download in enumerate(all_download_times):
-                        if np.isnan(download):
-                            all_download_times[i] = download_mean
-                    print("ufunktypeerror!!!")
-            data_to_keep['all_upload_times'] = all_upload_times
-            data_to_keep['all_prediction_times'] = all_prediction_times
-            data_to_keep['all_postprocess_times'] = all_postprocess_times
-            data_to_keep['all_download_times'] = all_download_times
-            aggregated_data.append(data_to_keep)
-    print(f'Total successes: {total_successes}')
-    print(f'Total failures: {total_failures}')
-    return aggregated_data
-
-def extra_network_costs(img_num, run_duration_minutes):
-    total_storage_gb = 1.5*img_num/1000
-    run_duration_months = run_duration_minutes/24/30
-
-    total_storage_cost = 0.026*total_storage_gb*run_duration_months
-    download_fees = 0.004*img_num/10000
-    publication_fees = 0.05*img_num/10000
-
-    total_fees = total_storage_cost + download_fees + publication_fees
-    return total_fees
-
 
 def get_time_by_gpu_data(raw_data, chosen_delay, img_nums):
     # only choose relevant runs
@@ -224,7 +50,6 @@ def get_time_by_gpu_data(raw_data, chosen_delay, img_nums):
             data_array[row,col] = output_data[ str(img_nums[var_num]) ][ var_index ][ row ]
     data_df = pd.DataFrame(data_array, columns=["10000","100000","10000_err","100000_err"], index=["1GPU","4GPU","8GPU"])
     return data_df
-
 
 def get_cost_by_gpu_data(raw_data, chosen_delay, chosen_img_num):
     # generate df
@@ -260,38 +85,6 @@ def get_cost_by_gpu_data(raw_data, chosen_delay, chosen_img_num):
             data_array[row,col] = output_data[ variables_of_interest[var_num] ][ var_index ][ row ]
     data_df = pd.DataFrame(data_array, columns=["total cost", "cpu node cost","gpu node cost","network costs","total_err","cpu_err","gpu_err","network_err"], index=["1GPU","4GPU","8GPU"])
     return data_df
-
-
-def format_data_for_error_plotting(data_lists):
-    means = []
-    std_errs_of_means = []
-    percentiles_975 = []
-    percentiles_025 = []
-    one_sided_95_percent_intervals = []
-    for datum_list in data_lists:
-        # compute mean
-        total = 0
-        for datum in datum_list:
-            total = total + datum
-        mean = total/len(datum_list)
-        means.append(mean)
-
-        # compute standard error of mean
-        squared_deviations = 0
-        for datum in datum_list:
-            squared_deviations = squared_deviations + (datum - mean)**2
-        std_dev = ( (1/(len(datum_list)-1)) * squared_deviations)**(1/2)
-        std_err_of_mean = std_dev/(len(datum_list)**(1/2))
-        std_errs_of_means.append(std_err_of_mean)
-
-        # compute 95% confidence intervals
-        percentile_975 = mean + (std_err_of_mean * 1.96)
-        percentile_025 = mean - (std_err_of_mean * 1.96)
-        percentiles_975.append(percentile_975)
-        percentiles_025.append(percentile_025)
-        one_sided_95_percent_intervals.append(std_err_of_mean * 1.96)
-    return (means, one_sided_95_percent_intervals)
-
 
 def plot_times(df, labels=None, title="multiple unstacked bar plot", y_label=None, H="/", pdf_name="output-bargraph.pdf", **kwargs):
     """Given a list of dataframes, with identical columns and index, create a clustered stacked bar plot. 
@@ -351,19 +144,11 @@ def plot_costs(df, labels=None, title="multiple unstacked bar plot", y_label=Non
     plt.savefig(pdf_name, transparent=True)
     return axe
 
-def define_colorblind_color_map():
-    colors = [(86/255,180/255,233/255), (0,158/255,115/255), (204/255,121/255,167/255), (213/255,94/255,0), (230/255,159/255,0)]  # (R,G,B) from nature methods Sky blue, bluish green, reddish purple, vermillion, orange
-    n_bins = 5
-    cmap_name = 'colorblind'
-    cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins)
-    return cm
-
 if __name__=='__main__':
     cm = define_colorblind_color_map()
 
     extracted_data = extract_data()
     raw_data = extracted_data
-
 
     delays = [5.0]
     #delays = [0.5, 5.0]
@@ -371,7 +156,6 @@ if __name__=='__main__':
     create_image_time_vs_gpu = True
     create_time_vs_gpu = True
     create_cost_vs_gpu = True
-
 
     for chosen_delay in delays:
         # create multiple-image-number graph
@@ -421,26 +205,6 @@ if __name__=='__main__':
                 except MissingDataError:
                     print(f"No data for gpu vs cost for delay {chosen_delay} and img_num {chosen_img_num}.")
                     pass
-
-
-
-
-
-
-    if False:
-        # create single-image-number graphs
-        if create_image_time_vs_gpu:
-            pass
-        if create_cost_vs_gpu:
-            # num_gpu vs. cost plot
-            try:
-                cost_vs_gpu_figure = CostVsGpu(raw_data, chosen_delay, chosen_img_num, title_label, pdf_label)
-                cost_vs_gpu_figure.plot()
-                print(f"Saved {cost_vs_gpu_figure.plot_pdf_name}")
-            except MissingDataError:
-                print(f"No data for gpu vs cost for delay {chosen_delay} and img_num {chosen_img_num}.")
-                pass
-    pdb.set_trace()
 
     if False:
         pdb.set_trace()
